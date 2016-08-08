22300db8c65981a7f370bee33341095669cc3fcf
bdf2ch <kolu0897@nw.mrsksevzap.ru>
2016-08-08 16:49:55 +0300

    temp

---
+++ js/source.js
@@ -0,0 +1,21 @@
+"use strict";
+
+
+ymaps.ready(init);
+var myMap,
+    myPlacemark;
+
+function init(){
+    myMap = new ymaps.Map("map", {
+        center: [55.76, 37.64],
+        zoom: 7,
+        controls: ["zoomControl"]
+    });
+
+    myPlacemark = new ymaps.Placemark([55.76, 37.64], {
+        hintContent: 'Москва!',
+        balloonContent: 'Столица России'
+    });
+
+    myMap.geoObjects.add(myPlacemark);
+}
